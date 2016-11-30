6934544a0e21a705992e5753c5c4d3c664561911
dearzoe <2421113739@qq.com>
2016-11-30 20:48:25 +0800

    banner

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let Nav = React.createClass({
+    render(){
+        let style={
+            "font-weight": "600",
+            "font-family":"Menlo, Monaco, Consolas, 'Courier New', monospace",
+            "font-style": "normal",
+            "font-size": "16px",
+        };
+        let styleActive={
+            "background":"#5bbc2e"
+        };
+            return <ul className="nav nav-pills" role="tablist">
+                        <li role="presentation" className="active" style={styleActive}><a href="#">主页</a></li>
+                        <li role="presentation"><a href="#">自我介绍</a></li>
+                        <li role="presentation"><a href="#">项目经验</a></li>
+                        <li role="presentation" className="dropdown">
+                            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+                                更多 <span className="caret"></span>
+                            </a>
+                            <ul style={style} className="dropdown-menu" role="menu">
+                                <li><a href="#">技术</a></li>
+                                <li><a href="#">未来规划</a></li>
+                                <li className="divider"></li>
+                                <li><a href="https://github.com/dearzoe"  style={style}>MyGitHub</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+    }
+});
+let Jumbotron = React.createClass({
+    render(){
+        let styleFuture={
+            "margin-top":"100px",
+            "text-align": "center",
+            "font-weight": "900",
+            "color": "#5bbc2e",
+            "font-size": "70px",
+            "line-height": "70px"
+        };
+        let styleResume={
+            "margin-top":"30px",
+            "text-align":"center"
+        };
+       return   <div className="jumbotron">
+           <h1 style={styleFuture}>Hello,MyFuture!</h1>
+           <p style={styleResume}>This is my resume for introduce myself~</p>
+           <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+       </div>
+    }
+});
+
+
+
+
+
+
+let AppMain=React.createClass({
+    render(){
+      return <div>
+                <div className="container">
+                   <Nav/>
+                </div>
+                <Jumbotron/>
+             </div>
+    }
+});
+
+ReactDOM.render(
+    <AppMain/>,
+    document.getElementById("app")
+);
